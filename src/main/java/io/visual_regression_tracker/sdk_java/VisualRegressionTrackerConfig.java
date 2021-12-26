61c37c9a232e335369d54090e4800399a62a8c17
Surat Das <surat.das@gmail.com>
2021-12-26 09:54:58 -0800

    Addresses issue #342 - Call VRT with minimum parameters.

--- src/main/java/io/visual_regression_tracker/sdk_java/VisualRegressionTrackerConfig.java
+++ src/main/java/io/visual_regression_tracker/sdk_java/VisualRegressionTrackerConfig.java
@@ -15,15 +15,15 @@ public class VisualRegressionTrackerConfig {
     @NonNull
     private final String apiUrl;
     @NonNull
-    private final String project;
-    @NonNull
     private final String apiKey;
     @NonNull
-    private final String branchName;
+    private final String project;
     @Builder.Default
-    private Boolean enableSoftAssert = false;
+    private String branchName = null;
     @Builder.Default
     private String ciBuildId = null;
+    @Builder.Default
+    private Boolean enableSoftAssert = false;
     @Builder.Default
     private int httpTimeoutInSeconds = 10;
 }
