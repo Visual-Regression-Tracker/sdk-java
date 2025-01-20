ebba8c81ceab68d4c0c2ffeeec610e974678cb7c
Oleg Ridchenko <oleg.ridchenko@globallogic.com>
2025-01-20 17:22:18 +0200

    make TestRunException available outiside of the package

--- src/main/java/io/visual_regression_tracker/sdk_java/TestRunException.java
+++ src/main/java/io/visual_regression_tracker/sdk_java/TestRunException.java
@@ -1,6 +1,6 @@
 package io.visual_regression_tracker.sdk_java;
 
-class TestRunException extends RuntimeException {
+public class TestRunException extends RuntimeException {
     public TestRunException(String errorMessage) {
         super(errorMessage);
     }
